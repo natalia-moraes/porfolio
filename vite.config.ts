d4f5c605888e71e360483346e370447a4b8c1695
Natalia Moraes <nmoraes@esapiens.com.br>
2023-11-15 15:31:33 -0300

    Instalar dependencia para svg

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
+import path from 'path'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
+  server: {
+    hmr: { host: 'localhost' },
+    origin: 'http://localhost:3000',
+    port: 3000,
+  },
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
 })
